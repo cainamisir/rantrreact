d4e25257e0075e7c1b03d9744d82110d1fffcdd6
Vlad Cainamisir <vlad.cainamisir@gmail.com>
2020-12-24 17:16:06 +0200

    v1 of frontend for matching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import Container from "react-bootstrap/Container";
+import ReactLoading from "react-loading";
+
+const io = require("socket.io-client");
+
+const ENDPOINT = "http://127.0.0.1:5000";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [response, setResponse] = useState("");
+    const [found, setFound] = useState(false);
+    useEffect(() => {
+        const socket = io(ENDPOINT, {
+            withCredentials: true,
+            extraHeaders: {
+                "my-custom-header": "abcd",
+            },
+        });
+        socket.on("chat begin", (data) => {
+            setFound(true);
+        });
+        socket.on("matchdisconnect", (data) => {
+            setFound(false);
+        });
+    }, []);
+
+    return (
+        <Container>
+            {found === false ? (
+                <>
+                    <h1> We are matching you right now to another ranter</h1>
+                    <ReactLoading
+                        type={"balls"}
+                        color={"blue"}
+                        height={"20%"}
+                        width={"50%"}
+                    />
+                </>
+            ) : (
+                <h1>Matched with a ranter</h1>
+            )}
+        </Container>
+    );
 }
 
 export default App;
